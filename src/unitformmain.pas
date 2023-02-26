b821cb2781fd055cabd077588e1f4fc129213e66
Noah Wood <noah@noahw.org>
2023-02-26 16:31:39 -0500

    Adds basic functionality.

--- src/unitformmain.pas
+++ src/unitformmain.pas
@@ -5,20 +5,60 @@ unit UnitFormMain;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  QlpIQrCode,
+  QlpQrCode,
+  QlpIQrSegment,
+  QlpQrSegment,
+  QlpQrSegmentMode,
+  QlpBitBuffer,
+  QlpConverters,
+  QlpQRCodeGenLibTypes,
+  fpwritebmp, fpreadbmp;
 
 type
-  TForm1 = class(TForm)
-  private
 
+  { TFormMain }
+
+  TFormMain = class(TForm)
+    ImageQRCode: TImage;
+    MemoInput: TMemo;
+    PanelTop: TPanel;
+    PanelBottom: TPanel;
+    Splitter: TSplitter;
+    procedure MemoInputChange(Sender: TObject);
+  private
+    procedure GenerateQR();
   public
 
   end;
 
+const
+  ECC_LEVEL=TQrCode.TEcc.eccLow;
+
 var
-  Form1: TForm1;
+  FormMain: TFormMain;
 
 implementation
+
+procedure TFormMain.MemoInputChange(Sender: TObject);
+begin
+  GenerateQR();
+end;
+
+procedure TFormMain.GenerateQR;
+var
+  InputText: String;
+  AQrCode: IQrCode;
+  ABitmap: TQRCodeGenLibBitmap;
+  AnEncoding: TEncoding;
+begin
+  InputText:=MemoInput.Lines.Text;
+  AnEncoding:=TEncoding.UTF8;
+  AQrCode:=TQrCode.EncodeText(InputText, ECC_LEVEL, AnEncoding);
+  ABitmap:=AQrCode.ToBitmapImage(1, 1);
+  ImageQRCode.Picture.Assign(ABitmap);
+end;
 
 {$R *.lfm}
 
